07b537262740c8a23c7fd92b008d5f86a5e654fa
filipemarraa <filipejacobsonmarra@gmail.com>
2024-08-19 18:21:23 -0300

    Arquivos alterados conforme o aprendizado de aulas novas

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+// Main.java
+import Sintaxe_Basica_com_Java.Anatomia_das_classes.MinhaClasse;
+
+public class Main {
+    public static void main(String[] args) {
+        MinhaClasse.main(args);  // Chama o método main da classe MinhaClasse
+        System.out.print("\nTeste1!");  // \n Pula linha
+    }
+}
---
+++ src/main/java/Sintaxe_Basica_com_Java/Anatomia_das_classes/MinhaClasse.java
@@ -0,0 +1,15 @@
+// MinhaClasse.java
+package Sintaxe_Basica_com_Java.Anatomia_das_classes;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.print("Opaaa!!! boa tarde!");
+
+        int ano = 2021;
+        ano = 2022;                 //Posso alterar a variavel INT
+
+        final String BR = "Brazil"; //Final = Nunca pode ser alterada
+
+        final double PI = 3.14;
+    }
+}
